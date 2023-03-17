a5bb7f0b3f6aefbe3031493fa5a6a2958c44efd3
Vorongor <mr.maddarknes@gmail.com>
2023-03-17 16:40:35 +0200

    Update 1_modal-order.js

--- src/js/1_modal-order.js
+++ src/js/1_modal-order.js
@@ -1,4 +1,4 @@
-// Відкриваємо перше модальне вікно
+// Відкриваємо перше модальне вікно Order
 var modalOrder = document.getElementById('modal-order');
 var btnOrder = document.getElementById('open-modal-order');
 var spanOrder = document.getElementsByClassName('close')[0];
@@ -12,7 +12,7 @@ spanOrder.onclick = function () {
   modalOrder.style.display = 'none';
 };
 
-// Відкриваємо друге модальне вікно при натисканні на кнопку в першому модальному вікні
+// Відкриваємо друге модальне вікно thanks при натисканні на кнопку в першому модальному вікні
 var modalThanks = document.getElementById('modal-thanks');
 var btnThanks = document.getElementById('open-modal-thanks');
 var spanThanks = document.getElementsByClassName('close')[1];
@@ -28,7 +28,7 @@ spanThanks.onclick = function () {
 };
 
 
-// Відкриваємо перше модальне вікно
+// Відкриваємо перше модальне вікно Review
 var modalReview = document.getElementById('modal-review');
 var btnReview = document.getElementById('open-modal-review');
 var spanReview = document.getElementsByClassName('close')[2];
@@ -42,7 +42,7 @@ spanReview.onclick = function () {
   modalReview.style.display = 'none';
 };
 
-// Відкриваємо друге модальне вікно при натисканні на кнопку в першому модальному вікні
+// Відкриваємо друге модальне вікно feedback при натисканні на кнопку в першому модальному вікні
 var modalFeedback = document.getElementById('modal-feedback');
 var btnFeedback = document.getElementById('open-modal-feedback');
 var spanFeedback = document.getElementsByClassName('close')[3];
