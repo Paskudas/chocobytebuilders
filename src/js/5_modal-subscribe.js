8b9fb4ebd91f18e278bda4533c39817a87b55252
Vorongor <mr.maddarknes@gmail.com>
2023-03-21 12:01:23 +0200

    Slider
    Prework-slider

--- src/js/5_modal-subscribe.js
+++ src/js/5_modal-subscribe.js
@@ -14,19 +14,3 @@ spanSubscribe.onclick = function () {
 
 
 
-
-
-// (() => {
-//     const refs = {
-//       openSubscribeBtn: document.querySelector("[data-subscribe-open]"),
-//       closeSubscribeBtn: document.querySelector("[data-subscribe-close]"),
-//       subscribe: document.querySelector("[data-subscribe]"),
-//     };
-  
-//     refs.openSubscribeBtn.addEventListener("click", toggleSubscribe);
-//     refs.closeSubscribeBtn.addEventListener("click", toggleSubscribe);
-  
-//     function toggleModal() {
-//       refs.subscribe.classList.toggle("is-hidden");
-//     }
-//   })();
